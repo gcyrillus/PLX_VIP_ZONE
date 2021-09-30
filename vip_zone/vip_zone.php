f9144d57df4e47b0b6aa4262b5a1d50ae58b3fe9
Grib Cyr <gccyrillus@hotmail.com>
2021-09-30 16:53:18 +0200

    Update vip_zone.php
    update des liens vers la racine de pluxml via PLX_ROOT

--- vip_zone/vip_zone.php
+++ vip_zone/vip_zone.php
@@ -29,8 +29,8 @@
 		$this->setAdminProfil(PROFIL_ADMIN);
         }
 
-		public function AdminPrepend() {
-            echo self::BEGIN_CODE;
+	public function AdminPrepend() {
+	    echo self::BEGIN_CODE;
 ?>
          const PROFIL_VISITOR = 5;  
 <?php
@@ -39,30 +39,30 @@
 
         public function OnDeactivate() {
             # code à exécuter à la désactivation du plugin, desactive les comptes visiteurs			
-			$deactivateVisitors=simplexml_load_file(PLX_ROOT.PLX_CONFIG_PATH."users.xml") or die("Error: Cannot create object");
-			foreach($deactivateVisitors->children() as $users) {
-			  if ( $users['profil'] =='5') {
-			  $users->attributes()->delete = '1';
-			  }
-			} 
-			$deactivateVisitors->asXml(PLX_ROOT.PLX_CONFIG_PATH."users.xml");
+	    $deactivateVisitors=simplexml_load_file(PLX_ROOT.PLX_CONFIG_PATH."users.xml") or die("Error: Cannot create object");
+		foreach($deactivateVisitors->children() as $users) {
+			if ( $users['profil'] =='5') {
+			$users->attributes()->delete = '1';
+			}
+		} 
+		$deactivateVisitors->asXml(PLX_ROOT.PLX_CONFIG_PATH."users.xml");
 	}
 
         public function OnActivate() {
             # code à exécuter à l’activation du plugin , réactive compte visiteur s'il y en a 
-			$reactivateVisitors=simplexml_load_file(PLX_ROOT.PLX_CONFIG_PATH."users.xml") or die("Error: Cannot create object");
-			foreach($reactivateVisitors->children() as $users) {
-			  if (( $users['profil'] =='5') and ( $users['delete'] =='1') ) {
-			  $users->attributes()->delete = '0';
-			  }
-			} 
-			$reactivateVisitors->asXml(PLX_ROOT.PLX_CONFIG_PATH."users.xml");				
+		$reactivateVisitors=simplexml_load_file(PLX_ROOT.PLX_CONFIG_PATH."users.xml") or die("Error: Cannot create object");
+		foreach($reactivateVisitors->children() as $users) {
+			if (( $users['profil'] =='5') and ( $users['delete'] =='1') ) {
+			$users->attributes()->delete = '0';
+			}
+		} 
+		$reactivateVisitors->asXml(PLX_ROOT.PLX_CONFIG_PATH."users.xml");				
         }
 
-		// on renvoi le VIP vers la page privée aprés authentification
+	// on renvoi le VIP vers la page privée aprés authentification
         public function AdminTopEndHead() {
             echo self::BEGIN_CODE;
-?>
+?>			
             if(isset($_SESSION['profil']) and $_SESSION['profil'] == '5') { header("location: ".$_COOKIE['page']);} 
 <?php
             echo self::END_CODE;
@@ -70,12 +70,12 @@
 
 	// on ajoute un profil utilisateur
         public function AdminUsersTop() {
-            echo self::BEGIN_CODE;
+	    echo self::BEGIN_CODE;
 ?>
-		//recuperation lang du plugin
-		$plxMotor = plxMotor::getInstance();
-		$plugin = $plxMotor->plxPlugins->aPlugins['vip_zone'];
-		$VIP_Profil = $plugin->getLang('L_PROFIL_VISITOR');
+
+	$plxMotor = plxMotor::getInstance();
+	$plugin = $plxMotor->plxPlugins->aPlugins['vip_zone'];
+	$VIP_Profil = $plugin->getLang('L_PROFIL_VISITOR');
 			
             # Tableau des profils
             $aProfils = array(
@@ -84,27 +84,27 @@
                 PROFIL_MODERATOR => L_PROFIL_MODERATOR,
                 PROFIL_EDITOR => L_PROFIL_EDITOR,
                 PROFIL_WRITER => L_PROFIL_WRITER,
-                PROFIL_VISITOR => $VIP_Profil
+                PROFIL_VISITOR => $VIP_Profil   
             );
 
 <?php
             echo self::END_CODE;
         } 
 
-		// on stocke la page qui a requis une authentification dans un cookie
+	// on stocke la page qui a requis une authentification dans un cookie
         public function AdminAuthEndHead() {
             echo self::BEGIN_CODE;
 ?>
-            if(isset($_GET['page'])) {setcookie("page",   $_GET['page']) ;}
+            if(isset($_GET['page']))   {setcookie("page",   $_GET['page']) ;}
 <?php
             echo self::END_CODE;
         }
 		
  
 
-		// avant l'affichage de la page, verifie si il y a besoin d'authenfication en comparant la configuration du plugin et le type de page demandée
+	// avant l'affichage de la page, verifie si il y a besoin d'authenfication en comparant la configuration du plugin et le type de page demandée
         public function ThemeEndHead() {	
-		            echo self::BEGIN_CODE;
+	    echo self::BEGIN_CODE;
 ?>
 		  # On recupere le mode privatisé
 		  $plxMotor = plxMotor::getInstance();
@@ -114,25 +114,22 @@
 		  #Selon le mode privatisé, on redirige vers la page d'authenfication si par encore logué
 		  if($parameter =="catart") {
 		        if (!isset($_SESSION['profil']) && ($plxMotor->mode !== 'home' ) && ($plxMotor->mode !== 'static' )) {
-					header("Location: /core/admin/auth.php?page=".$_SERVER['REQUEST_URI']);
+					header("Location: ".PLX_ROOT."core/admin/auth.php?page=".$_SERVER['REQUEST_URI']);
 				}
 		}
 		  else if ($parameter =="static") {
 				if (!isset($_SESSION['profil']) && ($plxMotor->mode === 'static' )) {
-					header("Location: /core/admin/auth.php?page=".$_SERVER['REQUEST_URI']);
+					header("Location: ".PLX_ROOT."core/admin/auth.php?page=".$_SERVER['REQUEST_URI']);
 				}
 		}	
 		  else if ($parameter =="blog") {
+			  // A FAIRE :verifier si il y a bien une page statique en acceuil, sinon ç'est une mise en orbite du visiteur
 				if (!isset($_SESSION['profil']) && ($plxMotor->mode !== 'static' )) {
-					header("Location: /core/admin/auth.php?page=".$_SERVER['REQUEST_URI']);
+					header("Location: ".PLX_ROOT."core/admin/auth.php?page=".$_SERVER['REQUEST_URI']);
 				}
 		}		
-		 
-		 
-		 
-		 
 <?php
-            echo self::END_CODE;
+	    echo self::END_CODE;
         }
 
 
@@ -140,9 +137,10 @@
         public function plxShowStaticListEnd() {	
 	    echo self::BEGIN_CODE;
 ?>
-		if (isset($_SESSION['profil'])) { array_push($menus, '<li class="static menu noactive"  id="VIP_logout"><a href="/core/admin/auth.php?d=1"  title="exit">X</a></li>');}
-		 
+		 if (isset($_SESSION['profil'])) { array_push($menus, '<li class="static menu noactive"  id="VIP_logout"><a href="'.PLX_ROOT.'core/admin/auth.php?d=1&logout=1"  title="exit">X</a></li>');}
 <?php
             echo self::END_CODE;
-        } 
+        }			
+
     }
+?>
